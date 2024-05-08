7dc9f2cb3f2d31982b4bccd383837fdbedd5d5e7
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-05-08 22:01:39 +0200

    error console

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,36 +3,49 @@ import { useId } from 'react';
 import css from './ContactForm.module.css';
 import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, selectContacts } from '../../redux/contactsSlice';
 
 const initialValues = {
     name: '',
     number: '',
 };
 
-const phoneRegExp = /^(\d{3}-\d{2}-\d{2})$/;
+const phoneRegExp = /^(?:\+\d{1,3})?(?:\d{9,15})$/;
 const contactSchema = Yup.object().shape({
     name: Yup.string()
-        .min(3, 'Too short!')
-        .max(30, 'Too long!')
-        .required('Required!'),
+        .min(3, 'Name is too short!')
+        .max(30, 'Name is too long!')
+        .required('Name is required!'),
     number: Yup.string()
         .matches(phoneRegExp, 'Phone number is not valid')
-        .required('Required!'),
+        .required('Phone number is required!'),
 });
 
-export default function ContactForm({ onAdd }) {
+export default function ContactForm() {
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
     const contactNameFieldId = useId();
     const numberFieldId = useId();
 
     const handleSubmit = (values, actions) => {
-        onAdd({
-        name: values.name,
-        number: values.number,
-        id: nanoid(),
-        });
-
-        actions.resetForm();
+    const isDuplicateNumber = contacts.some(
+        contact => contact.number === values.number
+        );
+        if (isDuplicateNumber) {
+        alert('This phone number already exists!');
+        } else {
+        dispatch(
+            addContact({
+            name: values.name,
+            number: values.number,
+            id: nanoid(),
+            })
+        );
+    }
+    actions.resetForm();
     };
+    
     return (
         <Formik
         initialValues={initialValues}
@@ -47,7 +60,7 @@ export default function ContactForm({ onAdd }) {
             <Field
                 type="text"
                 name="name"
-                id="contactNameFieldId"
+                id={contactNameFieldId}
                 className={css.input}
             />
             <ErrorMessage className={css.error} name="name" as="span" />
@@ -59,7 +72,7 @@ export default function ContactForm({ onAdd }) {
             <Field
                 type="text"
                 name="number"
-                id="numberFieldId"
+                id={numberFieldId}
                 className={css.input}
             />
             <ErrorMessage className={css.error} name="number" as="span" />
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,27 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectFilter } from '../../redux/filtersSlice';
 
-export default function ContactList({ contacts, onDeleteContact }) {
+
+export default function ContactList() {
+    const contacts = useSelector(selectContacts);
+    const filter = useSelector(selectFilter);
+    const getFilteredContacts = () => {
+        return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter ? filter.toLowerCase() : '')
+        );
+    };
+    const filteredContacts = getFilteredContacts();
     return (
         <ul className={css.list}>
-        {contacts.map(contact => (
-            <li key={contact.id} className={css.item}>
-            <Contact
-                name={contact.name}
-                number={contact.number}
-                onDelete={() => onDeleteContact(contact.id)}
-            />
+        {filteredContacts.map(({ id, name, number }) => (
+            <li key={id} className={css.item}>
+            <Contact name={name} number={number} id={id} />
             </li>
         ))}
         </ul>
     );
 }
+
